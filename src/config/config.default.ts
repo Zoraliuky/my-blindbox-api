533702607fea921c94636892319663bc7565e773
Zoraliuky <3158637286@qq.com>
2025-07-27 16:40:18 +0800

    Initial commit: Create MidwayJS backend project

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1753604148397_8647',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
