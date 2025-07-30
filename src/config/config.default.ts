322561b60f5aa0c33fdd6d57d2d64f6434e0be31
Zoraliuky <3158637286@qq.com>
2025-07-30 12:14:12 +0800

    fix(connection)

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,10 +1,9 @@
 import { MidwayConfig } from '@midwayjs/core';
 import { User } from '../entity/user.entity';
 import { BlindBox } from '../entity/blind-box.entity';
-import { Order } from '../entity/order.entity';  
+import { Order } from '../entity/order.entity';
 
 export default {
-  // 数据库配置
   typeorm: {
     dataSource: {
       default: {
@@ -12,13 +11,20 @@ export default {
         database: 'webapp.sqlite',
         synchronize: true,
         logging: true,
-        entities: [User,BlindBox,Order], // 确保这里有指定实体
+        entities: [User, BlindBox, Order],
       },
     },
   },
 
+  // 跨域(CORS)详细配置
+  crossDomain: {
+    origin: '*', // 允许所有来源的请求，用于开发和测试
+    allowHeaders: 'Content-Type,Authorization,X-Requested-With',
+    allowMethods: 'GET,POST,PUT,DELETE,OPTIONS',
+  },
+
   // Cookie 签名密钥
-  keys: 'lkyjsdmn_zytjsdcz_lkyhzytsybzdhpy', // 您的密钥
+  keys: 'lkyjsdmn_zytjsdcz_lkyhzytsybzdhpy',
 
   // 服务器端口
   koa: {
