f5c21b6a5f4a46945d237a54552ca42bd278fa14
Zoraliuky <3158637286@qq.com>
2025-08-01 14:56:44 +0800

    fix(package)

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,23 +2,16 @@ import { Configuration, App } from '@midwayjs/core';
 import * as koa from '@midwayjs/koa';
 import * as validate from '@midwayjs/validate';
 import * as info from '@midwayjs/info';
+import { join } from 'path';
 import * as typeorm from '@midwayjs/typeorm';
 import * as crossDomain from '@midwayjs/cross-domain';
 import * as staticFile from '@midwayjs/static-file';
 
-// 1. 直接导入我们的配置文件
-import defaultConfig from './config/config.default';
-import prodConfig from './config/config.prod';
-
 @Configuration({
-  // 2. 将导入的配置对象直接放在这里
-  //    这种方式不再依赖于文件系统扫描，打包后也能正常工作
-  importConfigs: [
-    {
-      default: defaultConfig,
-      prod: prodConfig,
-    }
-  ],
+  // importConfigs 的作用是告诉 MidwayJS 去扫描哪个文件夹来加载配置
+  // 这在开发环境和经典部署模式下是最佳实践
+  importConfigs: [join(__dirname, './config')],
+
   imports: [
     koa,
     validate,
